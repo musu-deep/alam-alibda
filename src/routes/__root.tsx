eb50643a6a03214295f549cfac03f6a2a4330300
LAIA <louiabdalla1@gmail.com>
2026-07-19 23:44:16 +0300

    Place legal advisor section after homepage hero

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,6 +11,7 @@ import { useEffect, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
+import { LegalAdvisorSection } from "../components/LegalAdvisorSection";
 
 function NotFoundComponent() {
   return (
@@ -119,6 +120,7 @@ function RootComponent() {
     <QueryClientProvider client={queryClient}>
       {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
       <Outlet />
+      <LegalAdvisorSection />
     </QueryClientProvider>
   );
 }
